afdccc07b3f31ac250164d2ffd7410b787f9e905
Nathan Robertson <absnathanr@gmail.com>
2018-10-20 15:30:11 -0500

    Added simple bot that scrapes images off a test subreddit and saves the last image it gets into a local jpg file

---
+++ bot.py
@@ -0,0 +1,22 @@
+import praw
+import urllib.request
+
+client_id = 'KOaqBIKzJYhhsg'
+client_secret = "sDRORt1IXXygdTmajuyGVOcm1B0"
+
+def main():
+	reddit = praw.Reddit(client_id=client_id,
+						 client_secret=client_secret,
+						 user_agent="Fuego Fire Tracker v 1.0 https://github.com/preyes0951/Minerva-SpotThatFire")
+	sub = reddit.subreddit('minervaTest')
+	for submission in sub.new():
+		# https://stackoverflow.com/questions/3042757/downloading-a-picture-via-urllib-and-python
+		image = urllib.request.urlopen(submission.url).read()
+		with open('image.jpg', 'wb') as f:
+			f.write(image)
+
+
+
+
+
+main()
